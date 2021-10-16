a5703998260593ed75da7c11799de39923d548ed
dgobalak <dgobalak@uwaterloo.ca>
2021-10-16 00:02:10 -0400

    Implemented Parser class for .pdf, .mp4, and .wav

--- src/inc/Parser.py
+++ src/inc/Parser.py
@@ -1,3 +1,48 @@
+import os
+import textract
+import moviepy.editor as mpe
 
 class Parser:
-    pass
+    def __init__(self, path):
+        self.path = path
+        self.text = self._parse_text()
+        self.ftype = self.get_ftype()
+        self.prepped_text = self._prep_text()
+
+    def get_text(self):
+        assert self.text != None
+        return self.prepped_text if self.prepped_text else self._prep_text(self.text)
+
+    def get_ftype(self):
+        _, fext = os.path.splitext(self.path)
+        ftypes = self.get_supported_ftypes()
+        assert fext in ftypes.keys()
+        return ftypes[fext]
+
+    def get_supported_ftypes(self):
+        return {
+            ".wav": "Audio",
+            ".mp3": "Audio",
+            ".mp4": "Video",
+            ".pdf": "PDF",
+            ".png": "Image",
+        }
+
+    def _prep_text(self):
+        text_prepped = self.text[2:-1].replace("\\r\\n", " ")
+        text_prepped = text_prepped.replace("\\n", " ")
+        text_prepped = text_prepped.replace("\\x0c", " ")
+        text_prepped = ' '.join(text_prepped.split()).strip()
+        return text_prepped
+
+    def _parse_text(self):
+        ftype = self.get_ftype()
+        if ftype == "PDF" or ftype == "Audio":
+            return str(textract.process(self.path))
+        elif ftype == "Video":
+            audio = mpe.VideoFileClip(self.path).audio
+            self.path = self.path[:-4] + ".wav"
+            audio.write_audiofile(filename=self.path)
+            return self._parse_text()
+        elif ftype == "Image":
+            pass
