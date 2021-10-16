ea886f7414be27a34b62492e9714fdc03c5e5771
dgobalak <dgobalak@uwaterloo.ca>
2021-10-16 03:05:38 -0400

    Created TextSummarizer class to inherit from.

---
+++ src/inc/text_summarizer.py
@@ -0,0 +1,9 @@
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+import re
+import heapq
+
+
+class TextSummarizer:
+    def __init__(self):
+        pass
--- src/inc/wiki_summarizer.py
+++ src/inc/wiki_summarizer.py
@@ -1,3 +1,8 @@
+from text_summarizer import TextSummarizer
+import urllib.request
+import bs4 as bs
 
-class WikiSummarizer:
-    pass
+
+class WikiSummarizer(TextSummarizer):
+    def __init__(self):
+        super().__init__()
