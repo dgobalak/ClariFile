0ca4b61773d79f7ccb9e43ce40fe2ad15cda82a1
Daniel Gobalakrishnan <63618806+dgobalak@users.noreply.github.com>
2021-10-31 02:25:33 -0400

    Completed file submission UI and back-end. (#30)

--- src/views.py
+++ src/views.py
@@ -1,9 +1,22 @@
 from src import app
+import os
 from flask import render_template, redirect, url_for, flash, request
+from werkzeug.utils import secure_filename
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if uploaded_file and allowed_file(uploaded_file.filename):
+            filename = secure_filename(uploaded_file.filename)
+            uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))       
+            return redirect(url_for('index'))
+        else:
+            print("File type not supported.")
     return render_template("index.html")
 
 
@@ -15,3 +28,9 @@ def internal_error(error):
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
+
+
+# Check if file type is supported
+def allowed_file(filename):
+    return '.' in filename and \
+        filename[len(filename) - 1 - filename[::-1].index("."):] in app.config['UPLOAD_EXTENSIONS']
