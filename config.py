cc8ed0f70f0d018314bd2f6acf971b4175cc659e
dgobalak <dgobalak@uwaterloo.ca>
2021-10-31 02:17:26 -0400

    Completed file submission UI and back-end.

--- config.py
+++ config.py
@@ -10,6 +10,12 @@ DEBUG = True
 # https://randomkeygen.com/
 SECRET_KEY = '6cOGFyylnBYVNs012bS4gjtWfsfds3423w3eqfea3d977d588'
 
+# File upload settings
+UPLOAD_EXTENSIONS = ['.wav', '.mp3', 'mp4', '.png', '.pdf']
+UPLOAD_FOLDER = "temp_storage/"
+# MAX_CONTENT_LENGTH = 1024 * 1024
+
+
 # Connect to the database
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
