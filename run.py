d498127458975ce72c52be29c828065492c4b8e5
dgobalak <dgobalak@uwaterloo.ca>
2021-12-19 01:53:42 -0500

    Serve website using waitress

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from src import app
+from waitress import serve
+import os
 
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    serve(app, host='0.0.0.0', port=port)
